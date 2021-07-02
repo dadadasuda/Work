d2ecd734883c3a693e6b74f3aa0d80c23b5312c8
dengl <abc.1234>
2021-07-02 10:57:32 +0800

    Initial commit

---
+++ src/com/dl/Thread/Volatile/VolatileVisbility.java
@@ -0,0 +1,37 @@
+package com.dl.Thread.Volatile;
+
+/**/
+public class VolatileVisbility {
+    /*加了volatile*/
+    //保证多个线程修改同一个共享变量能够感知
+    private static volatile boolean initfalse = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("waiting data ...");
+                while(!initfalse){
+
+                }
+                System.out.println("============success");
+            }
+        }).start();
+
+        Thread.sleep(2000);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                preparedate();
+            }
+        }).start();
+
+    }
+
+    public static void preparedate(){
+        System.out.println("prepareing data ...");
+        initfalse=true;
+        System.out.println("prepareing data end...");
+    }
+}
---
+++ src/com/dl/Thread/Volatile/VolatileVisbilityTest.java
@@ -0,0 +1,36 @@
+package com.dl.Thread.Volatile;
+
+/**/
+public class VolatileVisbilityTest {
+    /**/
+    private static  boolean initfalse = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("waiting data ...");
+                while(!initfalse){
+
+                }
+                System.out.println("============success");
+            }
+        }).start();
+
+        Thread.sleep(2000);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                preparedate();
+            }
+        }).start();
+
+    }
+
+    public static void preparedate(){
+        System.out.println("prepareing data ...");
+        initfalse=true;
+        System.out.println("prepareing data end...");
+    }
+}
